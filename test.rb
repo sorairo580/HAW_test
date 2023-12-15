7622a9c20f42ab3a79ac239b52fb22ca24c4ea47
sorairo580 <137414891+sorairo580@users.noreply.github.com>
2023-12-15 16:02:02 +0900

    test2回目

--- test.rb
+++ test.rb
@@ -1,16 +1,38 @@
-def ary_puts(ary)
-  ary = [0,1]
-  n = 2
-  while n <= 11
-    f = ary[(n-1)] + ary[(n-2)]
+def create_ary(count)
+  fib_ary = []
+
+  n = 0
+  while n < count
+    if n < 2
+      fib_ary << n
+    else
+      f = fib_ary[(n-1)] + fib_ary[(n-2)]
+      fib_ary << f
+    end
     n += 1
-    ary << f
   end
-puts ary
+
+  return fib_ary
 end
 
-ary_puts(12)
+puts create_ary(12)
 
+
+# def ary_puts(ary)
+#   ary = [0,1]
+#   n = 2
+#   while n <= 11
+#     f = ary[(n-1)] + ary[(n-2)]
+#     n += 1
+#     ary << f
+#   end
+# puts ary
+# end
+
+# ary_puts(12)
+
+
+# 技術試験問題
 # f(0) = 0
 # f(1) = 1
 # f(n) = f(n-1) + f(n-2) ただしn >= 2
